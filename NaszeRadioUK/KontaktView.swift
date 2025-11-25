03353b8decf3a1340628ed1f77eb9c8482630e1d
Daniel Żugaj <zugajdaniel@yahoo.com>
2025-11-25 19:44:40 +0000

    poprawne linki i wymuszenie "app open"

--- NaszeRadioUK/KontaktView.swift
+++ NaszeRadioUK/KontaktView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by user on 23/11/2025.
 //
-
 import SwiftUI
 
 struct KontaktView: View {
@@ -13,11 +12,18 @@ struct KontaktView: View {
     
     @State private var pulsate = false
 
-    // MARK: - Adresy do przekierowań
+    // MARK: - POPRAWIONE LINKI (Deep Links)
+    // WhatsApp: Bez zmian, działa dobrze
     private let whatsAppURL = URL(string: "https://wa.me/44777123456")!
-    private let facebookURL = URL(string: "https://www.facebook.com/naszeradiouk/")!
-    private let tiktokURL = URL(string: "https://www.tiktok.com/@naszeradio_uk?_r=1&_t=ZN-91hZNxiXpVm")!
-    private let instagramURL = URL(string: "https://www.instagram.com/naszeradiowuk/?igsh=NHFtdm8ybno1dG0w#")!
+    
+    // Facebook: Często lepiej działa bez 'www' lub ze specyficznym ID, ale zostawiamy standardowy jeśli działał
+    private let facebookURL = URL(string: "https://www.facebook.com/naszeradiouk")!
+    
+    // TikTok: Usunięcie 'www' często pomaga wymusić otwarcie aplikacji
+    private let tiktokURL = URL(string: "https://tiktok.com/@naszeradiouk")!
+    
+    // Instagram: Dodanie '/_u/' wymusza otwarcie w aplikacji na iOS!
+    private let instagramURL = URL(string: "https://instagram.com/_u/naszeradiouk")!
     
     var body: some View {
         ZStack {
@@ -28,66 +34,62 @@ struct KontaktView: View {
                 .ignoresSafeArea()
             
             // MARK: - Warstwa TREŚCI
-            VStack(spacing: 0) { // Używamy spacing: 0 i kontrolujemy odstępy paddingiem
+            VStack(spacing: 0) {
                 
-                // MARK: - NAGŁÓWEK (WYŚRODKOWANIE I ROZMIARY)
-                VStack(spacing: 5) { // Mały spacing dla zbliżenia linii
+                // MARK: - NAGŁÓWEK
+                VStack(spacing: 5) {
                     Text("Masz ciekawy temat?")
-                        .font(.largeTitle) // Rozmiar
+                        .font(.title2)
                     Text("Widzisz coś na drodze?")
-                        .font(.largeTitle) // Rozmiar
+                        .font(.title2)
                     Text("Napisz do nas:")
-                        .font(.title) // Rozmiar
+                        .font(.title3)
                         .fontWeight(.light)
                 }
                 .foregroundColor(.white)
                 .multilineTextAlignment(.center)
-                .padding(.top, 80) // Duży odstęp od góry
+                .padding(.top, 80)
 
                 
-                // MARK: - PRZYCISK WHATSAPP (Szerokość i Animacja)
-                // Używamy padding(top: 40) do oddzielenia od nagłówka
+                // MARK: - PRZYCISK WHATSAPP
                 Link(destination: whatsAppURL) {
                     Text("Wyślij wiadomość na WhatsApp")
                         .font(.headline)
                         .fontWeight(.bold)
                         .foregroundColor(.black)
-                        .padding(.vertical, 20) // Dodatkowy pionowy padding dla wysokości
-                        .frame(width: 300) // Stała, duża szerokość
+                        .padding(.vertical, 15)
+                        .frame(width: 300)
                         .background(
-                            RoundedRectangle(cornerRadius: 19)
+                            RoundedRectangle(cornerRadius: 10)
                                 .fill(Color.green)
                                 .scaleEffect(pulsate ? 1.05 : 1.0)
                         )
                 }
                 .padding(.top, 40)
-                
-                // Aktywacja animacji pulsowania
                 .onAppear {
                     withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
                         pulsate.toggle()
                     }
                 }
                 
-                // MARK: - LINKI DO SOCIAL MEDIÓW (Większy rozmiar i odstępy)
-                VStack(spacing: 40) { // Duży odstęp między linkami
+                // MARK: - LINKI DO SOCIAL MEDIÓW
+                VStack(spacing: 40) {
                     LinkText(title: "Facebook", url: facebookURL)
                     LinkText(title: "TikTok", url: tiktokURL)
                     LinkText(title: "Instagram", url: instagramURL)
                 }
-                .padding(.top, 90) // Duży odstęp od guzika WhatsApp
+                .padding(.top, 70)
                 
-                Spacer() // Pchnięcie stopki na dół
+                Spacer()
                 
                 // STOPKA
                 Text("Nasze Radio UK © 2025")
                     .font(.caption)
                     .foregroundColor(.gray)
-                    .padding(.bottom, 10) // Mały odstęp od dolnego paska
+                    .padding(.bottom, 10)
             }
-            .padding(.horizontal, 30) // Utrzymujemy poziome marginesy
+            .padding(.horizontal, 30)
             
-            // Ustawienie, by tło paska nawigacyjnego było czarne
             .toolbarColorScheme(.dark, for: .tabBar)
             
         } // Koniec ZStack
@@ -95,7 +97,6 @@ struct KontaktView: View {
 }
 
 // MARK: - Pomocniczy Widok dla Linków
-// Poprawiona czcionka i usunięte podkreślenie (na screenie jest tylko pogrubienie)
 struct LinkText: View {
     let title: String
     let url: URL
@@ -103,13 +104,13 @@ struct LinkText: View {
     var body: some View {
         Link(destination: url) {
             Text(title)
-                .font(.largeTitle) // ⬅️ Zwiększona czcionka
+                .font(.largeTitle)
                 .fontWeight(.semibold)
-            // .underline() // Usunięte podkreślenie, bo nie jest na screenie
                 .foregroundColor(.white)
         }
     }
 }
-    #Preview {
-        KontaktView(backgroundImageName: "HexagonBackground")
-    }
+
+#Preview {
+    KontaktView(backgroundImageName: "HexagonBackground")
+}
